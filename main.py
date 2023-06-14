d3ad40b5b6fc3fa02f2dc857ffb0a7ec2ecb6e75
Dan Duguay <Duguay9@gmail.com>
2023-06-13 23:51:52 -0400

    Found the means, started to graph

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 def extract_height_data(inputFile, outputFile, column_name):
     i = 0
     j = 0
@@ -40,6 +42,7 @@ def convert_FT_to_CM(inputFile, outputFile):
     inputFileVar.close()
     outputFileVar.close()
 
+# general population sample does not differentiate between male and female, so we will combine NBA and WNBA to match.
 def combine_WNBA_NBA (WNBAcmFile, NBAcmFile, outputFile):
     WNBAcmFileVar = open(WNBAcmFile, "r")
     NBAcmFileVar = open(NBAcmFile, "r")
@@ -57,6 +60,50 @@ def combine_WNBA_NBA (WNBAcmFile, NBAcmFile, outputFile):
     NBAcmFileVar.close()
     outputFileVar.close()
 
+# mean = 1/n * [sum of values from 1 to n]
+def calculate_mean (inputFile):
+    inputFileVar = open(inputFile, "r")
+    sum = 0
+    lineCount = 0
+
+    for line in inputFileVar:
+        if line.strip() != "":
+            sum += float(line)
+            lineCount += 1
+
+    print(sum)
+    print(lineCount)
+    print("\n")
+
+    return sum / lineCount
+
+def create_graph (inputFile, xAxis_name, yAxis_name, plotTitle):
+    inputFileVar = open(inputFile, "r")
+    y = [float(line.strip()) for line in inputFileVar]
+    x = [float(i) for i in range(1, len(y)+1)]
+
+    plt.plot(x,y)
+    plt.xlabel(xAxis_name)
+    plt.ylabel(yAxis_name)
+    plt.title(plotTitle)
+    plt.show()
+
+    inputFileVar.close()
+
+def sort_file (inputFile, outputFile):
+    inputFileVar = open(inputFile, "r")
+    outputFileVar = open(outputFile, "w")
+
+    lines = [line for line in inputFileVar]
+    lines.sort(key=float)
+
+    for line in lines:
+        outputFileVar.write(str(line))
+
+    inputFileVar.close()
+    outputFileVar.close()
+
+
 
 ####################################################################################################################
 
@@ -69,3 +116,20 @@ convert_FT_to_CM("NBAheightFTfile.txt", "NBAheightCMfile.txt")
 convert_FT_to_CM("WNBAheightFTfile.txt", "WNBAheightCMfile.txt")
 
 combine_WNBA_NBA("WNBAheightCMfile.txt", "NBAheightCMfile.txt", "NBAcombinedCMfile.txt")
+
+NBAmean = round(calculate_mean("NBAheightCMfile.txt"), 1)
+WNBAmean = round(calculate_mean("WNBAheightCMfile.txt"), 1)
+combinedMean = round(calculate_mean("NBAcombinedCMfile.txt"), 1)
+NHANESmean = round(calculate_mean("NHANESheightCMfile.txt"), 1)
+
+print("NBA mean: " + str(NBAmean) + "\n")
+print("WNBA mean: " + str(WNBAmean) + "\n")
+print("Combined NBA/WNBA mean: " + str(combinedMean) + "\n")
+print("NHANES mean: " + str(NHANESmean) + " (NOTE: Sample age range is 0 to 150 years old)\n")
+
+sort_file("NBAheightCMfile.txt", "sortedNBAcmFile.txt")
+sort_file("WNBAheightCMfile.txt", "sortedWNBAcmFile.txt")
+sort_file("NBAcombinedCMfile.txt", "sortedNBAcombinedFile.txt")
+sort_file("NHANESheightCMfile.txt", "sortedNHANEScmFile.txt")
+
+create_graph("sortedNBAcmFile.txt", "Number of People", "Height", "NBA Height In cm")
